917261685edb41f7a3320baaf764e345cb35a1eb
brenohof <brenohenriqueof@gmail.com>
2020-12-10 18:58:18 -0300

    login page

--- src/php/conexaoMysql.php
+++ src/php/conexaoMysql.php
@@ -4,9 +4,9 @@
 function mysqlConnect()
 {
   $db_host = "fdb30.awardspace.net";
-  $db_username = "";
-  $db_password = "";
-  $db_name = "";
+  $db_username = "3634944_ppi";
+  $db_password = "5K{VYU/T3@vZWi7L";
+  $db_name = "3634944_ppi";
 
   // dsn é apenas um acrônimo de database source name
   $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
