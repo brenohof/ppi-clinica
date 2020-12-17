55b267d03dcc28aa214d3ac077eb069121a72da6
brenohof <brenohenriqueof@gmail.com>
2020-12-17 14:07:17 -0300

    lista funcionarios

--- src/pages/private/index.php
+++ src/pages/private/index.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="../../style/private.css">
     <title>Clínica - Painel Adiministrativo</title>
 </head>
@@ -14,14 +13,12 @@
     <header>
         <nav class="p-4 navbar navbar-expand-md navbar-dark shadow">
             <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="justify-content-end collapse navbar-collapse" id="navbarNav">
                     <li class="nav-item dropdown">
-                        <a class="link nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="link nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Cadastrar
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -30,15 +27,14 @@
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="link nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="link nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Listar
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a prop-spa="./lista-paciente.html" class="dropdown-item" href="#">Pacientes</a></li>
-                            <li><a prop-spa="./lista-funcionario.html" class="dropdown-item" href="#">Funcionários</a></li>
+                            <li><a prop-spa="./lista-pacientes.php" class="dropdown-item" href="#">Pacientes</a></li>
+                            <li><a prop-spa="./lista-funcionarios.php" class="dropdown-item" href="#">Funcionários</a></li>
                             <li><a prop-spa="./lista-enderecos.php" class="dropdown-item" href="#">Endereços</a></li>
-                            <li><a prop-spa="./lista-agendamentos.html" class="dropdown-item" href="#">Agendamentos</a></li>
+                            <li><a prop-spa="./lista-agendamentos.php" class="dropdown-item" href="#">Agendamentos</a></li>
                         </ul>
                     </li>
                 </div>
@@ -47,6 +43,9 @@
     </header>
     <main class="mb-5">
         <div id="spa" class="container">
+            <?php
+            $login = true;
+            ?>
         </div>
     </main>
 
@@ -57,12 +56,8 @@
     <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
 
     <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
-        integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
-        integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ src/pages/private/lista-funcionarios.php
@@ -0,0 +1,70 @@
+<?php
+  require "../../php/conexaoMysql.php";
+  $pdo = mysqlConnect();
+  
+  try {
+  
+    $sql = <<<SQL
+    SELECT nome, email, telefone, cep, logradouro, bairro, cidade, estado,
+    data_contrato, salario, senha_hash
+    FROM pessoa INNER JOIN funcionario ON pessoa.codigo = funcionario.codigo
+    SQL;
+
+    $stmt = $pdo->query($sql);
+  } 
+  catch (Exception $e) {
+    exit('Ocorreu uma falha: ' . $e->getMessage());
+  }
+?>
+<div class="mt-5">
+<h3>Funcionários registrados</h3>
+    <table class="table table-striped">
+      <tr>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Telefone</th>
+        <th>CEP</th>
+        <th>Logradouro</th>
+        <th>Bairro</th>
+        <th>Cidade</th>
+        <th>Estado</th>
+        <th>Data contrato</th>
+        <th>Salário</th>
+        <th>Senha hash</th>
+      </tr>
+    <?php
+       while ($row = $stmt->fetch()) {
+
+        // Limpa os dados produzidos pelo usuário
+        // com possibilidade de ataque XSS
+        $logradouro = htmlspecialchars($row['logradouro']);
+        $bairro = htmlspecialchars($row['bairro']);
+        $cidade = htmlspecialchars($row['cidade']);
+        $estado = htmlspecialchars($row['estado']);
+        $cep = htmlspecialchars($row['cep']);
+        $nome = htmlspecialchars($row['nome']);
+        $email = htmlspecialchars($row['email']);
+        $telefone = htmlspecialchars($row['telefone']);
+        $dataContrato = htmlspecialchars($row['data_contrato']);
+        $salario = htmlspecialchars($row['salario']);
+        $senhaHash = htmlspecialchars($row['senha_hash']);
+
+        echo <<<HTML
+          <tr>
+            <td>$nome</td>
+            <td>$email</td>
+            <td>$telefone</td>
+            <td>$cep</td>
+            <td>$logradouro</td>
+            <td>$bairro</td>
+            <td>$cidade</td>
+            <td>$estado</td> 
+            <td>$dataContrato</td> 
+            <td>$salario</td> 
+            <td>$senhaHash</td> 
+          </tr>      
+        HTML;
+      }
+    ?>
+    </table>
+</div>
