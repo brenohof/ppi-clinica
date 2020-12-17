bb3a28e1e7d696db470282cfb9a11e2a8d843fff
brenohof <brenohenriqueof@gmail.com>
2020-12-17 18:29:26 -0300

    login

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,23 @@
-function checkLogin (e) {
-    e.preventDefault();
-    fetch('../php/login.php')
+function checkLogin(event) {
+    event.preventDefault();
+    const form = document.forms[0]
+    const formData = new FormData(form);
+
+    const options = {
+        method: 'post',
+        body: formData
+    }
+
+    fetch('../php/login.php', options)
         .then(response => response.json())
         .then(json => {
             if (!json.success) {
-                console.log(json)
+                document.getElementById('alerta').innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>${json.message}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>`
+            } else {
+                window.location.replace('./private/index.php');
             }
         })
 }
