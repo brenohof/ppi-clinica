7a31c6cdb78d4278404359707d5fd43a9a7ffa0e
brenohof <brenohenriqueof@gmail.com>
2020-12-17 13:54:07 -0300

    lista pacientes

--- src/pages/private/lista-pacientes.php
+++ src/pages/private/lista-pacientes.php
@@ -5,29 +5,32 @@
   try {
   
     $sql = <<<SQL
-    SELECT logradouro, bairro, cidade, estado, cep
-    FROM base_enderecos_ajax
+    SELECT nome, email, telefone, cep, logradouro, bairro, cidade, estado,
+    peso, altura, tipo_sanguineo
+    FROM pessoa INNER JOIN paciente ON pessoa.codigo = paciente.codigo
     SQL;
-  
-    // Neste exemplo não é necessário utilizar prepared statements
-    // porque não há possibilidade de injeção de SQL, 
-    // pois nenhum parâmetro é utilizado na query SQL
+
     $stmt = $pdo->query($sql);
   } 
   catch (Exception $e) {
-    //error_log($e->getMessage(), 3, 'log.php');
     exit('Ocorreu uma falha: ' . $e->getMessage());
   }
 ?>
 <div class="mt-5">
-<h3>Endereços registrados</h3>
+<h3>Pacientes registrados</h3>
     <table class="table table-striped">
       <tr>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Telefone</th>
         <th>CEP</th>
         <th>Logradouro</th>
         <th>Bairro</th>
         <th>Cidade</th>
         <th>Estado</th>
+        <th>Peso</th>
+        <th>Altura</th>
+        <th>Tipo sanguineo</th>
       </tr>
     <?php
        while ($row = $stmt->fetch()) {
@@ -39,14 +42,26 @@
         $cidade = htmlspecialchars($row['cidade']);
         $estado = htmlspecialchars($row['estado']);
         $cep = htmlspecialchars($row['cep']);
+        $nome = htmlspecialchars($row['nome']);
+        $email = htmlspecialchars($row['email']);
+        $telefone = htmlspecialchars($row['telefone']);
+        $peso = htmlspecialchars($row['peso']);
+        $altura = htmlspecialchars($row['altura']);
+        $tipoSanguineo = htmlspecialchars($row['tipo_sanguineo']);
 
         echo <<<HTML
           <tr>
+            <td>$nome</td>
+            <td>$email</td>
+            <td>$telefone</td>
             <td>$cep</td>
             <td>$logradouro</td>
             <td>$bairro</td>
             <td>$cidade</td>
             <td>$estado</td> 
+            <td>$peso</td> 
+            <td>$altura</td> 
+            <td>$tipoSanguineo</td> 
           </tr>      
         HTML;
       }
